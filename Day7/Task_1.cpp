b24ce036dd15723e71b300827aa5211b910fa405
omkar1772006-cmyk <omkar1772006@gmail.com>
2026-06-07 20:00:43 +0530

    Added Day7 recursive programs

---
+++ Day7/Task_1.cpp
@@ -0,0 +1,23 @@
+//Recursive function to calculate factorial of a number
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n == 0 || n == 1)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factorial = " << factorial(n);
+
+    return 0;
+}
---
+++ Day7/Task_3.cpp
@@ -0,0 +1,23 @@
+//Recursive function to calculate sum of digits of a number
+#include <iostream>
+using namespace std;
+
+int sumDigits(int n)
+{
+    if(n == 0)
+        return 0;
+
+    return (n % 10) + sumDigits(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Sum of digits = " << sumDigits(n);
+
+    return 0;
+}
