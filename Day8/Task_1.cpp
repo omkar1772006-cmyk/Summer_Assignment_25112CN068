dae6a917c86f0610f6333e2e203deffba15a3935
omkar1772006-cmyk <omkar1772006@gmail.com>
2026-06-09 22:11:10 +0530

    Added Day 8 and Day 9 pattern programs

---
+++ Day8/Task_1.cpp
@@ -0,0 +1,19 @@
+// Task 1: Print a right-angled triangle pattern of stars
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Day8/Task_4.cpp
@@ -0,0 +1,19 @@
+// Task 4: Print repeated numbers in a right-angled triangle pattern
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
